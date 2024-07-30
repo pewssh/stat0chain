d390843be45cd28532bd77c91b4b15f2d82b8374
pewssh <pewssh@gmail.com>
2024-07-30 23:40:58 +0545

    updated f

--- run_command.py
+++ run_command.py
@@ -1,8 +1,8 @@
 import subprocess
 
 commands = []
-for i in range(16,20):
-    commands.append(["python3", "main.py", str(i), "1", "10", "5", "40"])
+for i in range(1,10):
+    commands.append(["python3", "main.py", "1", str(i), "10", "5", "40"])
 
 for i in commands:
     subprocess.run(i)
